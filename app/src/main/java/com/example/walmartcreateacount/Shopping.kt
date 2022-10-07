3a200cbd9dee85ad61a470f5aff5304e9a23649f
Wendmagegn Kebede <wendmagegn.b@gmail.com>
2022-10-07 10:18:38 -0500

    final

---
+++ app/src/main/java/com/example/walmartcreateacount/Shopping.kt
@@ -0,0 +1,11 @@
+package com.example.walmartcreateacount
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Shopping : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
